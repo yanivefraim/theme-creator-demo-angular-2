9c1ec0c6863559d82d95d73fed7b2b0524b28341
Yaniv Efraim <yaniv.efraim@gmail.com>
2015-08-31 18:24:08 +0300

    remove unused code

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,6 @@
 import {Component, View, bootstrap} from 'angular2/angular2';
 import {RouteConfig, RouterOutlet, RouterLink, routerInjectables} from 'angular2/router';
 
-//import {FontColorComponent} from './components/font-color-component/font-color-component';
 import {ThemeCreatorComponent} from './components/theme-creator-component/theme-creator-component';
 
 @Component({
